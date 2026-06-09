d603aa40c81a2f29b838eda1cf916c15930c94bb
carme-hp <homspons@gmail.com>
2026-06-09 14:20:13 +0200

    Rename material

--- AdapterPlugin.cpp
+++ AdapterPlugin.cpp
@@ -2,27 +2,27 @@
 
 #include <FECore/sdk.h>
 #include "PreciceCallback.h"
-#include "DiHuMaterial.h"
+#include "GammaMaterial.h"
 
 FECORE_PLUGIN int GetSDKVersion()
 {
 	return FE_SDK_VERSION;
 }
 
-FECORE_PLUGIN void GetPluginVersion(int &major, int &minor, int &patch)
+FECORE_PLUGIN void GetPlugInVersion(int &major, int &minor, int &patch)
 {
 	major = 1;	
 	minor = 0;
 	patch = 0;
 }
 
-FECORE_PLUGIN void PluginInitialize(FECoreKernel &fecore)
+FECORE_PLUGIN void PlugInInitialize(FECoreKernel &fecore)
 {
 	FECoreKernel::SetInstance(&fecore);
 	// register adapter callback
-   	REGISTER_FECORE_CLASS(PreciceCallback, "precice_callback");
+   	REGISTER_FECORE_CLASS(PreciceCallback, "preCICEAdapter");
 	// register custom material
-    	REGISTER_FECORE_CLASS(DiHuMaterial, "DiHuMaterial");
+    	REGISTER_FECORE_CLASS(GammaMaterial, "GammaMaterial");
 	// register custom contraction
-	REGISTER_FECORE_CLASS(DiHuContraction, "DiHuContraction");
+	REGISTER_FECORE_CLASS(gammaContraction, "gammaContraction");
 }
